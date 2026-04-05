1f4675a5e0a0e4c31fbd3221a8b513797269ae19
Prohor722 <prohor.b722@gmail.com>
2026-04-05 23:33:14 +0600

    add note feature done

--- addNote.go
+++ addNote.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func addNote() {
-	fmt.Println("This is the addNote function.")
+	fmt.Println("Add new note..")
+	fmt.Println("Enter note title:")
+	var title string
+	fmt.Scanln(&title)
+	fmt.Println("Enter note body:")
+	var body string
+	fmt.Scanln(&body)
+	note = struct {
+		id  int
+		title string
+		body  string
+	}{id: len(notes) + 1, title: title, body: body}
+	notes = append(notes, struct {
+		note struct {
+			id  int
+			title string
+			body  string
+		}
+	}{note: note})
+	fmt.Println("Note added successfully!")
 }
--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+var note struct {
+	id   int
+	title string
+	body  string
+}
+
+var notes []struct {
+	note struct {
+		id  int
+		title string
+		body  string
+	}
+}
+
 func main() {
 	test()
 	fmt.Println("Hello, World!")
