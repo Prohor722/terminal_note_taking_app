0adfebdcb43099909fee85bca4985edf796a4d55
Prohor722 <prohor.b722@gmail.com>
2026-04-06 16:08:37 +0600

    some issues fixed

--- selectOption.go
+++ selectOption.go
@@ -6,16 +6,21 @@ func selectOption() {
 	switch option {
 	case 1:
 		addNote()
+		pickOption()
 	case 2:
 		viewNotes()
+		pickOption()
 	case 3:
 		deleteNote()
+		pickOption()
 	case 4:
 		editNote()
+		pickOption()
 	case 5:
 		fmt.Println("Exiting the Note Taking App...")
 		return
 	default:
 		fmt.Println("Invalid option. Please try again.")
+		pickOption()
 	}
 }
