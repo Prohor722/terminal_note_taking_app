bc777891b5bed7e24e0c8a53eeed546db7cbf825
Prohor722 <prohor.b722@gmail.com>
2026-04-06 16:05:54 +0600

    editNote function done

---
+++ editNote.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func editNote() {
+	fmt.Println("Edit note..")
+	fmt.Println("Enter note id to edit:")
+	var id int
+	fmt.Scanln(&id)
+	for i, n := range notes {
+		if n.note.id == id {
+			fmt.Println("Enter new note title:")
+			var title string
+			fmt.Scanln(&title)
+			fmt.Println("Enter new note body:")
+			var body string
+			fmt.Scanln(&body)
+			notes[i].note.title = title
+			notes[i].note.body = body
+			fmt.Println("Note updated successfully!")
+			return
+		}
+	}
+	fmt.Println("Note not found!")
+}
