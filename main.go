3bd2ec23c68989ef1805f2f1a20551c420e6eec4
Prohor722 <prohor.b722@gmail.com>
2026-04-06 15:53:58 +0600

    select option done

--- main.go
+++ main.go
@@ -16,6 +16,17 @@ var notes []struct {
 	}
 }
 
+var option = 0
+
 func main() {
 	fmt.Println("Welcome to the Note Taking App!")
+
+	fmt.Println("Select a option:")
+	fmt.Println("1. Add Note")
+	fmt.Println("2. View Notes")
+	fmt.Println("3. Delete Note")
+	fmt.Println("4. Edit Note")
+	fmt.Println("5. Exit")
+
+
 }
---
+++ selectOption.go
@@ -0,0 +1,19 @@
+package main
+
+func selectOption() {
+	switch option {
+	case 1:
+		addNote()
+	case 2:
+		viewNotes()
+	case 3:
+		deleteNote()
+	case 4:
+		editNote()
+	case 5:
+		fmt.Println("Exiting the Note Taking App...")
+		return
+	default:
+		fmt.Println("Invalid option. Please try again.")
+	}
+}
