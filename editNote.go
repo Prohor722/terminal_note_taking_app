f585d67fc9183f0c1399902c60fbd1d488c62ecf
Prohor722 <prohor.b722@gmail.com>
2026-04-08 22:33:33 +0600

    some issues fixed

--- editNote.go
+++ editNote.go
@@ -8,12 +8,12 @@ func editNote() {
 	var id int
 	fmt.Scanln(&id)
 	for i, n := range notes {
-		if n.note.id == id {
+		if n.id == id {
 			title := addTitle()
 			body := addBody()
-			
-			notes[i].note.title = title
-			notes[i].note.body = body
+
+			notes[i].title = title
+			notes[i].body = body
 			fmt.Println("Note updated successfully!")
 			return
 		}
