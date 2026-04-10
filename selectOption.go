31c4427e66cf1c78df9be7041d67d4d5dfe9e2e5
Prohor722 <prohor.b722@gmail.com>
2026-04-10 23:44:31 +0600

    some issues fixed

--- selectOption.go
+++ selectOption.go
@@ -30,25 +30,6 @@ func selectOption(option int) bool {
     return true
 }
 
-// func selectOption(option int) bool {
-//     switch option {
-//     case 1:
-//         caseCall(addNote)
-//     case 2:
-//         caseCall(viewNotes)
-//     case 3:
-//         caseCall(deleteNote)
-//     case 4:
-//         caseCall(editNote)
-//     case 5:
-//         fmt.Println("Exiting the Note Taking App...")
-//         return false
-//     default:
-//         fmt.Println("Invalid option. Please try again.")
-//     }
-//     return true
-// }
-
 func caseCall(f func()) {
 	f()
 	pickOption()
