574015c68f296db6375d3ac07ede19490e9af648
Prohor722 <prohor.b722@gmail.com>
2026-04-10 23:41:02 +0600

    some issues fixed

--- selectOption.go
+++ selectOption.go
@@ -2,41 +2,51 @@ package main
 
 import "fmt"
 
+const (
+    OptionAdd    = 1
+    OptionView   = 2
+    OptionDelete = 3
+    OptionEdit   = 4
+    OptionExit   = 5
+)
+
+var actions = map[int]func(){
+    OptionAdd:    addNote,
+    OptionView:   viewNotes,
+    OptionDelete: deleteNote,
+    OptionEdit:   editNote,
+}
+
 func selectOption(option int) bool {
-    switch option {
-    case 1:
-        caseCall(addNote)
-    case 2:
-        caseCall(viewNotes)
-    case 3:
-        caseCall(deleteNote)
-    case 4:
-        caseCall(editNote)
-    case 5:
+    if option == OptionExit {
         fmt.Println("Exiting the Note Taking App...")
         return false
-    default:
+    }
+    if action, ok := actions[option]; ok {
+        caseCall(action)
+    } else {
         fmt.Println("Invalid option. Please try again.")
     }
     return true
 }
 
-// func selectOption() {
-// 	switch option {
-// 	case 1:
-// 		caseCall(addNote)
-// 	case 2:
-// 		caseCall(viewNotes)
-// 	case 3:
-// 		caseCall(deleteNote)
-// 	case 4:
-// 		caseCall(editNote)
-// 	case 5:
-// 		fmt.Println("Exiting the Note Taking App...")
-// 		return
-// 	default:
-// 		fmt.Println("Invalid option. Please try again.")
-// 	}
+// func selectOption(option int) bool {
+//     switch option {
+//     case 1:
+//         caseCall(addNote)
+//     case 2:
+//         caseCall(viewNotes)
+//     case 3:
+//         caseCall(deleteNote)
+//     case 4:
+//         caseCall(editNote)
+//     case 5:
+//         fmt.Println("Exiting the Note Taking App...")
+//         return false
+//     default:
+//         fmt.Println("Invalid option. Please try again.")
+//     }
+//     return true
 // }
 
 func caseCall(f func()) {
