7bcc4718e258017a19c029eb9fd916f298cfac10
Prohor722 <prohor.b722@gmail.com>
2026-04-12 23:29:21 +0600

    some issues fixed

--- editNote.go
+++ editNote.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func addTitle() string {
+	var title string
+	fmt.Print("Enter title: ")
+	fmt.Scanln(&title)
+	return title
+}
+
 func editNote() {
 	fmt.Println("Edit note..")
 	fmt.Println("Enter note id to edit:")
